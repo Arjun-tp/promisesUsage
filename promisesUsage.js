77e479188da22255653d3f99b50b72f86332b549
arjun tp <arjun.tp@medibox.in>
2017-11-24 19:49:30 +0530

    simple promise function

--- promisesUsage.js
+++ promisesUsage.js
@@ -2,31 +2,32 @@
  * Created by arjun on 24/11/17.
  */
 
-var flag = false;
+var isLeaveConfirmed = false; //This is the variable which describes the behavior of promise function
 
-var promiseVariable = new Promise(function (resolve,reject) {
-    if(flag){
-        var query = {
-            name : 'XYZ',
-            age : 12
+var canGoForTrip = new Promise(function (resolve,reject) {
+    if(isLeaveConfirmed){
+        var tripTo = {
+            place : 'Goa',
+            days : 5
         };
+        resolve(tripTo);
     }else {
-        var reason = new Error('Not Possible');
+        var reason = new Error('Tasks not yet completed');
         reject(reason);
     }
 });
 
 
-var mainFunc = function () {
-    promiseVariable
-        .then(function (fullfilled) {
-            console.log("Done!!!!!");
+var askForLeave = function () {
+    canGoForTrip
+        .then(function (fulfilled) {
+            console.log("Leaves are confirmed",fulfilled);
         })
         .catch(function (error) {
-            console.log(error.message);
+            console.log(error.message);//error.message returns only the message
         })
 };
 
 
 
-mainFunc();
+askForLeave();
